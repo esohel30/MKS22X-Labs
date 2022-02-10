f133e26a17fe017c851a89295ecd47efe7555b81
esohel30 <esohel30@stuy.edu>
2022-02-09 22:43:46 -0500

    RecursionClasswork i have the base case as of right now for my helper method for split array

--- classwork/codingbat.java
+++ classwork/codingbat.java
@@ -10,21 +10,30 @@ public class codingbat{
       return false;
 
     }else{
+      //choose or dont choose
       return (groupSum(start + 1, nums, target - nums[start])  || groupSum(start + 1, nums, target) );
     }
   }
 
   public static boolean splitArray(int[] nums) {
-
-
   }
+
+    public static boolean part(int[] nums, int index, int a, int b ){
+      if(nums.length  == index){
+        return a == b;
+
+        
+
+    }
+
+
 
 
 
 
   public static void main(String[] args) {
       System.out.println( groupSum(1, new int[]{2,4,8} , 12) );
-
+      System.out.println( splitArray(new int[] {1,2,3,5}));
    }
 
 
