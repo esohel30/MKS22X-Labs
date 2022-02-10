ca79a713f0bf08cc62b47bb8a783e3021765ed71
esohel30 <esohel30@stuy.edu>
2022-02-09 23:30:46 -0500

    RecursionClasswork added the base cases for the groupsum6 question. copy and pasted from og groupsum base cases and added one for encountering 6

--- classwork/codingbat.java
+++ classwork/codingbat.java
@@ -2,13 +2,11 @@ public class codingbat{
 
 
   public static boolean groupSum(int start, int[] nums, int target) {
-
     if(target == 0){
       return true;
     }
     if(start == nums.length || target < 0){
       return false;
-
     }else{
       //choose or dont choose
       return (groupSum(start + 1, nums, target - nums[start])  || groupSum(start + 1, nums, target) );
@@ -22,10 +20,23 @@ public class codingbat{
     public static boolean part(int[] nums, int index, int a, int b ){
       if(nums.length  == index){
         return a == b;
-
         }else{
         return ( part(nums, index + 1, a + nums[index], b) || part(nums, index + 1, a, b + nums[index]) );
       }
+    }
+
+    public static boolean groupSum6(int start, int[] nums, int target){
+      if(target ==0){
+        return true;
+      }
+      if(start == nums.length || target < 0){
+        return 0;
+      }
+      if(nums[index] == 6){
+        return groupSum6(start + 1, nums, target - 6);
+      }
+
+
 
     }
 
@@ -35,16 +46,16 @@ public class codingbat{
 
 
   public static void main(String[] args) {
-      System.out.println( groupSum(1, new int[]{2,4,8} , 12) );
-      System.out.println( splitArray(new int[] {1,2,3,5}));
-      System.out.println( splitArray(new int[] {1,3434,3,5}));
-      System.out.println( splitArray(new int[] {1,2,3344}));
-      System.out.println( splitArray(new int[] {1,2,3,4,5,6,7,8,9,10}));
-      System.out.println( splitArray(new int[] {1,2,3,4,5,6,7,8,9,10,11}));
-      System.out.println( splitArray(new int[] {0}));
-      System.out.println( splitArray(new int[] {1}));
-      System.out.println( splitArray(new int[] {}));
-
+      // System.out.println( groupSum(1, new int[]{2,4,8} , 12) );
+      // System.out.println( splitArray(new int[] {1,2,3,5}));
+      // System.out.println( splitArray(new int[] {1,3434,3,5}));
+      // System.out.println( splitArray(new int[] {1,2,3344}));
+      // System.out.println( splitArray(new int[] {1,2,3,4,5,6,7,8,9,10}));
+      // System.out.println( splitArray(new int[] {1,2,3,4,5,6,7,8,9,10,11}));
+      // System.out.println( splitArray(new int[] {0}));
+      // System.out.println( splitArray(new int[] {1}));
+      // System.out.println( splitArray(new int[] {}));
+  
 
 
 
