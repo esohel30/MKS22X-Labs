a187ee757a425800fca59d57d079da11baee69a6
esohel30 <esohel30@stuy.edu>
2022-02-09 20:55:49 -0500

    RecursionClasswork added the testcases for the groupsum problem and they seem to work; gona transfer to coding bat now

--- classwork/codingbat.java
+++ classwork/codingbat.java
@@ -1,7 +1,7 @@
 public class codingbat{
 
 
-  public boolean groupSum(int start, int[] nums, int target) {
+  public static boolean groupSum(int start, int[] nums, int target) {
 
     if(target == 0){
       return true;
@@ -10,9 +10,20 @@ public class codingbat{
       return false;
 
     }else{
-      return (groupSum(start + 1, nums, target - nums[start])  || groupSum(start + 1, nums, target));
+      return (groupSum(start + 1, nums, target - nums[start])  || groupSum(start + 1, nums, target) );
     }
   }
+
+
+  public static void main(String[] args) {
+
+      System.out.println( groupSum(1, new int[]{2,4,8} , 12) );
+      System.out.println( groupSum(0, new int[]{12,23,23,23,23,23,23} , 46) );
+      System.out.println( groupSum(1, new int[]{212,4,8} , 11) );
+      System.out.println( groupSum(1, new int[]{2,4,238} , 244) );
+      System.out.println( groupSum(1, new int[]{2,423,8} , 1112) );
+
+   }
 
 
 
