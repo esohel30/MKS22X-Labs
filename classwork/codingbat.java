09ad0cf30a309d8e5c1a5dbee635f59bda30a7f9
esohel30 <esohel30@stuy.edu>
2022-02-09 20:45:57 -0500

    RecursionClasswork added the recursive bit: you can either choose or no chose and that will lead to all combinations plus 3 single picks.

--- classwork/codingbat.java
+++ classwork/codingbat.java
@@ -9,6 +9,13 @@ public class codingbat{
     if(start == nums.length){
       return false;
 
+    }else{
+      return (groupSum(start + 1, nums, target - nums[start])  || groupSum(start + 1, nums, target));
+    }
+  }
+
+
+
 
 
 }
