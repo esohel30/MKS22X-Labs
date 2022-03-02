922770b4b5a2ac088ba31ae00fcf1081b3f80885
esohel30 <esohel30@stuy.edu>
2022-03-01 21:01:03 -0500

    adding all the maze files + worked on reading in a file

--- 03-maze/ReadFile.java
+++ 03-maze/ReadFile.java
@@ -1,17 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+
 public class ReadFile {
     public static int[] getValuesFromFile(String filename) throws FileNotFoundException {
+        String info = "" ;
         File text = new File(filename);
-        //code here to read file into array
+        Scanner input = new Scanner(text);
+
+        while(input.hasNextLine()){
+          String temp = input.nextLine();
+          info += temp + "\n";
+        }
+
+        System.out.println(info);
+        return new int[0];
+
     }
 
     public static void main(String args[]) {
         int[] nums = new int[0];
         try {
-            nums = getValuesFromFile("Numbers.csv");
+            nums = getValuesFromFile("");
         } catch (FileNotFoundException e) {
-            //error handling code
+            System.out.print(e);
         }
     }
+  }
