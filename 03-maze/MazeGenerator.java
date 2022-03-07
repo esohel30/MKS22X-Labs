54e8b877ffd543a9695093691135b6f0298d9487
esohel30 <esohel30@stuy.edu>
2022-03-06 22:30:24 -0500

    worked on the toString for the Maze generator class

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,7 +1,19 @@
 import java.util.*;
 
-public class MazeGenerator{
+public class MazeGenerator {
 
+    public static String toString(char[][] prototype) {
+        String temp = "";
+        for (int a = 0; a < prototype.length; a++) {
+            for (int b = 0; b < prototype[a].length; b++) {
+                temp += prototype[a][b];
+            }
+            if (a != prototype.length - 1) {
+                temp = temp + "\n";
+            }
+        }
+        return temp;
+    }
 
 
 }
