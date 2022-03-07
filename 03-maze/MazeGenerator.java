ac917f28b7fe60a49bb38d3ae40bd4643093ac65
esohel30 <esohel30@stuy.edu>
2022-03-07 00:28:53 -0500

    added a method that randomly picks directions to place and a places a blank spot there. Code used to shuffle an array probaly could be cut down but it works and i am testing now

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.util.Arrays;
+import java.util.Random;
+
 public class MazeGenerator {
 
     public static void filler(char[][] proto) {
@@ -22,6 +25,15 @@ public class MazeGenerator {
         return temp;
     }
 
+    public static void randompick(String[] moves, char[][] protos, int y, int x) {
+        for (int a = 0; a < 4; a++) {
+            if (moves[a] == "down")  placer(protos, y - 1, x);
+            if (moves[a] == "left")  placer(protos, y, x + 1);
+            if (moves[a] == "up")    placer(protos, y + 1, x);
+            if (moves[a] == "right") placer(protos, y, x - 1);
+        }
+    }
+
     public static boolean goodSpot(char[][] proto, int y, int x, int tot) {
         if (proto[y][x + 1] == ' ') tot += 1;
         if (proto[y + 1][x] == ' ') tot += 1;
@@ -34,12 +46,32 @@ public class MazeGenerator {
     public static boolean badSpot(char[][] proto, int y, int x) {
         int temp = 0;
 
-        if (y == 0 || x == 0) return false;
-        if (x == proto[0].length - 1 || y == proto.length - 1) return false;
+        if (y == 0 || x == 0) return false; // out of bounds
+        if (x == proto[0].length - 1 || y == proto.length - 1) return false; // out of bounds
         if (proto[y][x] == ' ') return false;
 
-        return goodSpotCheck(proto, y, x, temp);
+        return goodSpot(proto, y, x, temp); // empty spots around
+    }
+
+    public static void placer(char[][] proto, int y, int x) {
+        if (badSpot(proto, y, x)) {
+
+            String[] randomized = { "up","right",  "down",  "left" };
+
+            Random rand = new Random();
+
+            for (int i = 0; i < randomized.length; i++) {
+                int rover = rand.nextInt(randomized.length);
+                String temp = randomized[rover];
+                randomized[rover] = randomized[i];
+                randomized[i] = temp;
+            }
+
+            proto[y][x] = ' ';
+            randompick(randomized, proto, y, x);
+        }
     }
 
 
-  }
+    
+}
