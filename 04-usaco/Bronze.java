7d1275a2a37df0fe65228650968365672381a8cb
esohel30 <esohel30@stuy.edu>
2022-03-09 11:01:04 -0500

    created constructer and working on converting stuff inside input file into variables. basically working on the contructor

---
+++ 04-usaco/Bronze.java
@@ -0,0 +1,40 @@
+public class Bronze{
+  private int R, C, E, N;
+  private int[][] pasture;
+  private int R_s, C_s, D_sl
+  
+
+
+
+  public farm(String filename){
+    File texter = new File(filename);
+    Scanner input = new Scanner(texter);
+
+    while (input.hasNextLine()) {
+        String temp = input.nextLine();
+        if (!temp.equals("")) { // ignore pesky empty lines that show up at the ends.
+            values.add(temp);
+        }
+
+
+
+
+  }
+
+
+
+  public static void main(String[] args) {
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
