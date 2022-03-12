2086486de015007846321d7155948cdf4064bd29
esohel30 <esohel30@stuy.edu>
2022-03-12 17:10:39 -0500

    created the constructor for the silver problem

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -1,28 +1,45 @@
-public class silver{
+import java.util.*;
+import java.io.*;
+
+public class Silver{
   private int N, M, T;
   private char[][] pasture;
   private int R1, C1, R2, C2;
 
 
-  public pasture(String filename){
+  public Silver(String filename) throws FileNotFoundException{
     File texter = new File(filename);
     Scanner input = new Scanner(texter);
-    ArrayList < String > values = new ArrayList < String > ();
 
-    while (input.hasNextLine()) {
-        String temp = input.nextLine();
-        if (!temp.equals("")) {
-            values.add(temp);
-        }
+    N = input.nextInt();
+    M = input.nextInt();
+    T = input.nextInt();
+    pasture = new char[N][M];
+
+    for(int i = 0; i < N; i ++){
+      String x = input.next();
+      for(int j = 0; j < M; j++){
+        pasture[i][j] = x.charAt(j);
       }
-      System.out.println(temp);
-  }
+    }
+
+      R1 = input.nextInt();  C1 = input.nextInt();
+      R2 = input.nextInt();  C2 = input.nextInt();
+
+    }
+
 
 
 
 
   public static void main(String[] args) {
+    try{
+      Silver x = new Silver("sampleInput");
 
+
+    }catch(FileNotFoundException e){
+        System.out.println(e);
+    }
   }
 
 
