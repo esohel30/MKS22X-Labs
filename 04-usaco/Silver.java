478546bead44d7f8db0fb52aa79b910a6bc7b7f5
esohel30 <esohel30@stuy.edu>
2022-03-12 21:59:48 -0500

    worked on scanning the pasture in for the silver problem and then fixed errors with having a constructor

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -1,47 +1,54 @@
 import java.util.*;
 import java.io.*;
 
-public class Silver{
-  private int N, M, T;
-  private char[][] pasture;
-  private int R1, C1, R2, C2;
+public class Silver {
+    public static long solve(String filename) {
+
+        try {
+            File texter = new File(filename);
+            Scanner input = new Scanner(texter);
+
+            int N, M, T;
+            char[][] pasture;
+            int R1, C1, R2, C2;
+
+            N = input.nextInt();
+            M = input.nextInt();
+            T = input.nextInt();
+            pasture = new char[N][M];
+
+            for (int i = 0; i < N; i++) {
+                String x = input.next();
+                for (int j = 0; j < M; j++) {
+                    pasture[i][j] = x.charAt(j);
+                }
+            }
+
+            R1 = input.nextInt();
+            C1 = input.nextInt();
+            R2 = input.nextInt();
+            C2 = input.nextInt();
 
 
-  public Silver(String filename) throws FileNotFoundException{
-    File texter = new File(filename);
-    Scanner input = new Scanner(texter);
+            for (int a = 0; a < N; a++) {
+                for (int b = 0; b < M; b++) {
+                    System.out.print(pasture[a][b] + " ");
+                }
+                System.out.print("\n");
 
-    N = input.nextInt();
-    M = input.nextInt();
-    T = input.nextInt();
-    pasture = new char[N][M];
+            }
 
-    for(int i = 0; i < N; i ++){
-      String x = input.next();
-      for(int j = 0; j < M; j++){
-        pasture[i][j] = x.charAt(j);
-      }
+
+            return 1;
+        } catch (FileNotFoundException e) {
+            return -1;
+        }
     }
 
-    R1 = input.nextInt();  C1 = input.nextInt();
-    R2 = input.nextInt();  C2 = input.nextInt();
-  }
 
-
-  public static void main(String[] args) {
-    try{
-      Silver x = new Silver("sampleInput");
-
-
-    }catch(FileNotFoundException e){
-        System.out.println(e);
+    public static void main(String[] args) {
+        solve("ctravel.1.in");
     }
-  }
-
-
-
-
-
 
 
 
