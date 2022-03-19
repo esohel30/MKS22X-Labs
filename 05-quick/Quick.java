e196feea991ac3f80d9e4fc7c296cfdafad50ce8
esohel30 <esohel30@stuy.edu>
2022-03-19 16:18:49 -0400

    working on partition and created a helper method that helps swap array positions so i dont have to repeat code

---
+++ 05-quick/Quick.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Quick {
+
+    public static void s(int[] data, int a, int b) { //swaps two indexes of an array
+        int shift = data[a];
+        data[a] = data[b];
+        data[b] = shift;
+    }
+
+
+    
+
+
+
+}
