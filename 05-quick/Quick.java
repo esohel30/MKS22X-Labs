5b3db05561d1995f4fc8919288db0331f64bd051
esohel30 <esohel30@stuy.edu>
2022-03-19 16:20:42 -0400

    finished working on partition and now going to test it with different types of arrays: negatives and reversed and sorted arrays

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -9,8 +9,22 @@ public class Quick {
     }
 
 
-    
+    public static int partition(int[] data, int lo, int hi) {
+        s(data, (int)(Math.random() * ((hi + 1) - lo) + lo), lo);
+        int piv = lo + 1;
 
+        for (int i = lo + 1; i < hi + 1; i++) {
+            if (data[i] <= data[lo]) {
+                s(data, piv, i);
+                piv++;
+            }
+        }
+
+        s(data, piv - 1, lo);
+        return piv - 1;
+    }
+
+    
 
 
 }
