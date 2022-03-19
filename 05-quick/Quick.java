509e8f746226b91ba66099eceddd06592469d34f
esohel30 <esohel30@stuy.edu>
2022-03-19 16:23:43 -0400

    testing partition and added print statements to the code to see the before and after. testing on easily viewable arrays with 999 in the start and end

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -12,6 +12,7 @@ public class Quick {
     public static int partition(int[] data, int lo, int hi) {
         s(data, (int)(Math.random() * ((hi + 1) - lo) + lo), lo);
         int piv = lo + 1;
+        System.out.println(piv);
 
         for (int i = lo + 1; i < hi + 1; i++) {
             if (data[i] <= data[lo]) {
@@ -21,10 +22,22 @@ public class Quick {
         }
 
         s(data, piv - 1, lo);
+        System.out.println(Arrays.toString(data));
         return piv - 1;
     }
 
-    
+
+
+
+
+    public static void main(String[] args) {
+      partition( new int[] {4,3,2,1,0}, 0, 4);
+      partition( new int[] {4,3,2,1,0,997,998,999}, 0, 4);
+      partition( new int[] {997,998,999,4,3,2,1,0},3,7);
+      partition( new int[] {993,994,995,4,3,2,1,0,997,998,999},3,7);
+    }
+
+
 
 
 }
