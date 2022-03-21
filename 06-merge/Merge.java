8eebbe8d340d068133de02f68e26a31768289482
esohel30 <esohel30@stuy.edu>
2022-03-21 19:36:01 -0400

    finished working on merge arrarys method and tested on 3 different types of arrays

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -7,41 +7,34 @@ public class Merge{
     int len1 =  left.length-1;
     int len2 = right.length-1;
 
-    while(len1 != 0 && len2 != 0) {
-
-      if(len1 > 0 && len2 == 0){
+    while(i != -1) {
+      if(len1 > -1 && len2 == -1){
         temp[i] = left[len1];
         len1 --;
         i--;
       }
-
-      if(len2 > 0 && len1 == 0){
+      if(len2 > -1 && len1 == -1){
         temp[i] = right[len2];
         len2 --;
         i--;
       }
-
-      if(left[len1] >= right[len2]){
+      if(len1 > -1 && len2 > -1 && left[len1] >= right[len2]){
         temp[i] = left[len1];
         len1 --;
         i--;
       }
-      if(left[len1] <= right[len2]){
+      if(len1 > -1 && len2 > -1 && left[len1] <= right[len2]){
         temp[i] = right[len2];
         len2 --;
         i--;
       }
-
-
     }
-
     return temp;
-
   }
 
   public static void main(String[] args) {
-    int[] x = {1,2,3,4,5};
-    int[] y = {1,2,3,4,5};
+    int[] x = {1,2,3,4,5,6,7,8,9,10};
+    int[] y = {11,12,13,14,15};
     System.out.println(Arrays.toString(merge(x,y)));
 
 
