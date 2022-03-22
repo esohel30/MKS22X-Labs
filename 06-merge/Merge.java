221b4b73613eb8868c000f98573e79bd62bdeae4
esohel30 <esohel30@stuy.edu>
2022-03-22 10:57:25 -0400

    tested my merge method again that merges two arrays torgether and though of a better way that can reduce the amount of statements. Also I worked on merge sort and so far was able to split array into left and right

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,43 +1,100 @@
 import java.util.Arrays;
 public class Merge{
+        /*merge takes two sorted arrays and returns a new array that combines all
+         *elements of both arrays in asorted order.
+         *@left a sorted array (this is a precondition)
+         *@right a sorted array (this is a precondition)
+         *@return a sorted array that contains all elements of left and right
+         */
+        public static int [] merge(int [] left, int[] right){
+          int i = left.length + right.length -1;
+          int[] temp = new int[i+1];
+          int len1 =  left.length-1;
+          int len2 = right.length-1;
 
-  public static int [] merge(int [] left, int[] right){
-    int i = left.length + right.length -1;
-    int[] temp = new int[i+1];
-    int len1 =  left.length-1;
-    int len2 = right.length-1;
+          while(i != -1) {
 
-    while(i != -1) {
-      if(len1 > -1 && len2 == -1){
-        temp[i] = left[len1];
-        len1 --;
-        i--;
-      }
-      if(len2 > -1 && len1 == -1){
-        temp[i] = right[len2];
-        len2 --;
-        i--;
-      }
-      if(len1 > -1 && len2 > -1 && left[len1] >= right[len2]){
-        temp[i] = left[len1];
-        len1 --;
-        i--;
-      }
-      if(len2 > -1 && len1 > -1 && left[len1] <= right[len2]){
-        temp[i] = right[len2];
-        len2 --;
-        i--;
-      }
-    }
-    return temp;
-  }
+            if(len1 > -1 && len2 == -1){
+              temp[i] = left[len1];
+              len1 --;
+              i--;
+            }
+            if(len2 > -1 && len1 == -1){
+              temp[i] = right[len2];
+              len2 --;
+              i--;
+            }
+            if(len1 > -1 && len2 > -1 && left[len1] >= right[len2]){
+              temp[i] = left[len1];
+              len1 --;
+              i--;
+            }
+            if(len2 > -1 && len1 > -1 && left[len1] <= right[len2]){
+              temp[i] = right[len2];
+              len2 --;
+              i--;
+            }
 
-  public static void main(String[] args) {
-    int[] x = {1,2,3,4,5,6,7,8,9,10};
-    int[] y = {0,0,0,0,0,0,0,0,0,0,0,0,11,12,13,14,15,16,17,18,19,2000,2000,2000,2000,2000};
-    System.out.println(Arrays.toString(merge(x,y)));
+          }
+          System.out.print(Arrays.toString(temp));
+          return temp;
+        }
+
+        /*mergesortH is the actual mergesort method.
+        *@data the array to be sorted
+        *@return a new array that is the sorted version of data.
+        */
+        public static int[] mergesortH(int [] data){
 
 
-  }
+          int len = data.length;
 
-}
+          if(len == 1){
+            System.out.print(Arrays.toString(data));
+          }
+
+          if(len > 1){
+            int half = data.length/2;
+            int[] left = new int[half];
+            int[] right = new int[len - half];
+            for(int i = 0; i < half; i ++){
+              left[i] = data[i];
+            }
+            for(int j = 0; j < half; j++){
+              right[j] = data[j + half];
+            }
+
+            mergesortH(left);
+            mergesortH(right);
+
+
+          }
+
+
+
+          return new int[1]; //so this compiles
+        }
+
+        /*mergesort uses the recursive mergesortH method to create a sorted
+        *version of the array. It then copies the data back into the original
+        *array. (This is for compatibility with prior sort testers)
+        *@param data the array to be sorted, this will be modified by the method
+        */
+        public static void mergesort(int [] data){
+          int [] temp  =  mergesortH(data);
+          for(int i = 0; i < data.length; i++){
+            data[i] = temp[i];
+          }
+        }
+
+        public static void main(String[] args) {
+          int[] x = {1,2,3,4,5,6,7};
+          int[] y = {1,23,56,78};
+
+          //System.out.print(Arrays.toString(merge(x,y)));
+
+          //System.out.print(len/2);
+          mergesortH(x);
+        }
+
+        }
