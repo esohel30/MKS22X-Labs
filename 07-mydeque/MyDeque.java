378f04704ca3bfc8470696eb8d9840a1f550a051
esohel30 <esohel30@stuy.edu>
2022-03-29 22:56:32 -0400

    finished working on tostring and tested it throughly adn now proceeding to work on my change size method which will resize my aray

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -28,6 +28,44 @@ public class MyDeque<E> {
     return size;
   }
 
+  public String toString() {
+    String temp = "{";
+    int front = this.f;
+    boolean swit = false;
+    int len = data.length;
+
+   if (this.size == 0){
+     return temp + "}";
+   }
+
+   // keep going until a null pops up
+   while (this.data[front] != null) {
+
+     if (front == this.f && swit){
+       return temp + "}";
+     }
+
+     //obtains front and palces
+     temp += this.data[front];
+     front = front + 1;
+     temp += ", ";
+     swit = true;
+
+     if (front == len){
+       front = 0;
+     }
+
+   }
+
+   int l = temp.length();
+
+   if (swit){
+     temp = temp.substring(0, l - 2) + "}";
+   }
+
+   return temp;
+ }
+
 
 
 }
--- 07-mydeque/tester.java
+++ 07-mydeque/tester.java
@@ -3,6 +3,17 @@ import java.util.Arrays;
 public class tester {
   public static void main(String[] args) {
     MyDeque<Integer> test = new MyDeque<Integer>(5);
+    System.out.println(test);
 
-  
+
+    test.addFirst(12);
+    System.out.println(test);
+
+    test.addLast(12);
+    test.addFirst(1);
+    test.addFirst(1);
+
+    System.out.println(test);
+    System.out.println(Arrays.toString(test.data));
+  }
 }
