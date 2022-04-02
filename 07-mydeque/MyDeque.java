0c9c039c9a84feecb6858b659da7dc11f10d591c
esohel30 <esohel30@stuy.edu>
2022-04-01 22:32:35 -0400

    fixed minor syntax errors and logic errors with unchecked errors and small error with toString where it went out of bounds sometimes. Also fixed small error with change size method

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -1,29 +1,30 @@
-
 import java.util.*;
 
 public class MyDeque < E > {
 
     @SuppressWarnings("unchecked")
     public E[] data;
-    public int size,f,b;
+    public int size,
+    f,
+    b;
     // f for front. b for back
 
     public MyDeque() {
         @SuppressWarnings("unchecked")
         E[] d = (E[]) new Object[16];
         this.data = d;
-        f = -1;
-        b = -1;
-        size = 0;
+        this.size = 0;
+        this.f = -1;
+        this.b = -1;
     }
 
     public MyDeque(int initialCapacity) {
         @SuppressWarnings("unchecked")
         E[] d = (E[]) new Object[initialCapacity];
         this.data = d;
-        f = -1;
-        b = -1;
-        size = 0;
+        this.size = 0;
+        this.f = -1;
+        this.b = -1;
     }
 
     public int size() {
@@ -39,70 +40,66 @@ public class MyDeque < E > {
         if (this.size == 0) {
             return temp + "]";
         }
-
         // keep going until a null pops up
         while (this.data[front] != null) {
-
             int l = temp.length();
-            if (front == this.f && swit) {
+            if (front == this.f && swit == true) {
 
                 // return temp without the additional space and comma
                 return temp.substring(0, l - 2) + "]";
             }
-
             //obtains front and palces
             temp += this.data[front];
             front = front + 1;
             temp += ", ";
             swit = true;
-
             if (front == len) {
                 front = 0;
             }
         }
-        int l = temp.length();
-        if (swit) {
-            temp = temp.substring(0, l - 2) + "]";
+        int s = temp.length() + 1;
+
+        if (front == this.f && swit == true) {
+            temp = temp.substring(0, s - 3) + "]";
         }
         return temp;
     }
 
+    @SuppressWarnings("unchecked")
     private void changeSize() {
         int l = data.length;
+        int front = this.f; // no accidental changes
         boolean full = (size == l);
-
         if (l == 0) {
             @SuppressWarnings("unchecked")
             E[] temp = (E[]) new Object[20];
             data = temp;
+            // change adress of array after copying
         } else if (full) {
             @SuppressWarnings("unchecked")
             E[] temp = (E[]) new Object[l + l];
-
-            temp[0] = data[this.f];
+            temp[0] = data[front];
             int counter = 0;
             int counter1 = f;
             counter++;
-
             for (int j = counter; j < l; j++) {
                 counter1++;
                 if (counter1 == l) {
-                    counter1 = 0;
+                    counter1 -= counter1;
                 }
                 temp[counter] = this.data[counter1];
-                counter++;
+                counter += 1;
             }
-
-            this.f = 0;
+            counter -= 1;
+            this.f -= front;
             this.data = temp;
-            this.b = counter - 1;
+            this.b = counter;
         }
     }
 
 
 
     public void addFirst(E element) {
-
         if (element == null) {
             throw new NullPointerException("must enter a value for this method");
         }
@@ -125,28 +122,25 @@ public class MyDeque < E > {
     }
 
     public void addLast(E element) {
-    if (element == null){
-      throw new NullPointerException("must enter a value for this method");
+        if (element == null) {
+            throw new NullPointerException("must enter a value for this method");
+        }
+        changeSize();
+        int s = data.length;
+        if (this.b == -1) {
+            this.f = this.b;
+            this.data[0] = element;
+            this.f = this.f - this.b;
+            this.b = this.f;
+        } else if (this.b + 1 == s) {
+            b -= b;
+            data[0] = element;
+        } else {
+            b += 1;
+            data[b] = element;
+        }
+        this.size += 1; // increment size by one because of the added element
     }
-    changeSize();
-    int s = data.length;
-
-    if (this.b == -1) {
-      this.f = this.b;
-      this.data[0] = element;
-      this.f = this.f - this.b;
-      this.b = this.f;
-    }
-    else if (this.b + 1 == s) {
-      b -= b;
-      data[0] = element;
-    }
-    else {
-      b += 1;
-      data[b] = element;
-    }
-    this.size += 1; // increment size by one because of the added element
-  }
 
     public E getFirst() {
         boolean temp = this.f == -1;
@@ -172,52 +166,44 @@ public class MyDeque < E > {
 
     public E removeFirst() {
         int l = data.length;
+        int s = this.size;
 
         if (this.data[f] == null) {
             throw new NoSuchElementException("this element does not exist");
         }
-
         if (this.f == -1) {
             throw new NoSuchElementException("this element does not exist");
         }
-        this.size = this.size - 1;
-
+        s--;
+        this.size = s;
         E temp = this.data[this.f];
         // holds value that will be removed
-
         data[this.f] = null;
         this.f = this.f + 1;
-
         if (this.f == l) {
             this.f = this.f - l;
         }
-
         return temp;
     }
 
     public E removeLast() {
         int l = data.length - 1;
-
+        int s = this.size;
         if (this.data[b] == null) {
             throw new NoSuchElementException("this element does not exist");
         }
-
         if (this.f == -1) {
             throw new NoSuchElementException("this element does not exist");
         }
-        this.size = this.size - 1;
-
+        s--;
+        this.size = s;
         E temp = this.data[this.b];
         // holds value that will be removed
-
         this.data[b] = null;
         this.b = this.b - 1;
-
-        if (this.b == -1) {
+        if (this.b < 0 && this.b == -1) {
             this.b = l;
         }
-
         return temp;
     }
-
 }
